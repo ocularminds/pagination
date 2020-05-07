5767408c421fff3eb0750ddfae3c16a8700cd84b
Festus B. Jejelowo <mail.festus@gmail.com>
2020-05-07 16:15:51 +0100

    updated pagination events

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './app.css';
+import Countries from 'countries-api';
+import CountryCard from './components/country';
+import Pagination from './components/pagination';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    this.state = {
+      allCountries: [],
+      currentCountries: [],
+      currentPage: null,
+      totalPages: null,
+    };
+    this.onPageChanged = this.onPageChanged.bind(this);
+  }
+  componentDidMount() {
+    const {data: allCountries = []} = Countries.findAll();
+    this.setState({allCountries});
+  }
+
+  onPageChanged(data) {
+    const {allCountries} = this.state;
+    const {currentPage, totalPages, limit} = data;
+    const offset = (currentPage - 1) * limit;
+    const currentCountries = allCountries.slice(offset, offset + limit);
+    this.setState({currentPage, currentCountries, totalPages});
+  }
+
+  render() {
+    const {
+      allCountries,
+      currentCountries,
+      currentPage,
+      totalPages,
+    } = this.state;
+    const totalCountries = allCountries.length;
+
+    if (totalCountries === 0) return null;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Custom react components: CountryCard and Pagination
+            <br />
+            CountryCard: For display country names and flags.
+            <br />
+            Pagination: For paginating large data set for display.
+          </p>
+        </header>
+        const headerClass = ['text-dark py-2 pr-4 m-0', currentPage ?
+        'border-gray border-right' : ''].join(' ').trim(); return (
+        <div className="container mb-5">
+          <div className="row d-flex flex-row py-5">
+            <div className="w-100 px-4 py-5 d-flex flex-row flex-wrap align-items-center justify-content-between">
+              <div className="d-flex flex-row align-items-center">
+                <h2 className={headerClass}>
+                  <strong className="text-secondary">{totalCountries}</strong>{' '}
+                  Countries
+                </h2>
+                {currentPage && (
+                  <span className="current-page d-inline-block h-100 pl-4 text-secondary">
+                    Page <span className="font-weight-bold">{currentPage}</span>{' '}
+                    / <span className="font-weight-bold">{totalPages}</span>
+                  </span>
+                )}
+              </div>
+              <div className="d-flex flex-row py-4 align-items-center">
+                <Pagination
+                  totalRecords={totalCountries}
+                  limit={18}
+                  nextPages={1}
+                  onPageChanged={this.onPageChanged}
+                />
+              </div>
+            </div>
+
+            {currentCountries.map((country) => (
+              <CountryCard key={country.cca3} country={country} />
+            ))}
+          </div>
+        </div>
+        );
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/paginate.js
@@ -0,0 +1,195 @@
+import React from 'react';
+
+const LEFT_PAGE = 'LEFT';
+const RIGHT_PAGE = 'RIGHT';
+
+/**
+ * Helper method for creating a range of numbers
+ * range(1, 5) => [1, 2, 3, 4, 5]
+ */
+const range = (from, to, step = 1) => {
+  let i = from;
+  const range = [];
+
+  while (i <= to) {
+    range.push(i);
+    i += step;
+  }
+
+  return range;
+};
+
+class Paginate extends React.Component {
+  construction(props) {
+    super(props);
+    const {records = 0, limit = 10} = props;
+    this.limit = typeof limit === 'number' ? limit : 10;
+    this.records = typeof records === 'number' ? records : 0;
+    this.totalPages = Math.ceil(this.records / this.limit);
+    this.state = {currentPage: 1};
+  }
+
+  componentDidMount() {
+    getPage(1);
+  }
+
+  getPage(page){
+    const {onPageChanged = f => f } = this.props;
+    const currentPage = Math.max(0, Math.min(0,this.totalPages));
+    const data = {
+      currentPage, 
+      totalPages: this.totalPages,
+      limit: this.limit,
+      records = this.records
+    };
+    this.setState({currentPage}, ()=> onPageChanged(data))
+  }
+
+  handleClick(page, event) {
+    event.preventDefault();
+    getPage(page);
+  }
+
+  handleNext(e) {
+    e.preventDefault();
+    this.getPage(this.currentPage + 1);
+  }
+
+  handlePrevious(e) {
+    e.preventDefault();
+    this.getPage(this.state.currentPage - 1);
+  }
+  /**
+   * Let's say we have 10 pages and we set nextPages to 2
+   * Given that the current page is 6
+   * The pagination control will look like the following:
+   *
+   * (1) < {4 5} [6] {7 8} > (10)
+   *
+   * (x) => terminal pages: first and last page(always visible)
+   * [x] => represents current page
+   * {...x} => represents page neighbours
+   */
+  fetchPageNumbers = () => {
+    const totalPages = this.totalPages;
+    const currentPage = this.state.currentPage;
+
+    /**
+     * totalNumbers: the total page numbers to show on the control
+     * totalBlocks: totalNumbers + 2 to cover for the left(<) and right(>) controls
+     */
+    const totalNumbers = this.nextPages * 2 + 3;
+    const totalBlocks = totalNumbers + 2;
+
+    if (totalPages > totalBlocks) {
+      const startPage = Math.max(2, currentPage - pageNeighbours);
+      const endPage = Math.min(totalPages - 1, currentPage + pageNeighbours);
+      let pages = range(startPage, endPage);
+
+      /**
+       * hasLeftSpill: has hidden pages to the left
+       * hasRightSpill: has hidden pages to the right
+       * spillOffset: number of hidden pages either to the left or to the right
+       */
+      const hasLeftSpill = startPage > 2;
+      const hasRightSpill = totalPages - endPage > 1;
+      const spillOffset = totalNumbers - (pages.length + 1);
+
+      switch (true) {
+        // handle: (1) < {5 6} [7] {8 9} (10)
+        case hasLeftSpill && !hasRightSpill: {
+          const extraPages = range(startPage - spillOffset, startPage - 1);
+          pages = [LEFT_PAGE, ...extraPages, ...pages];
+          break;
+        }
+
+        // handle: (1) {2 3} [4] {5 6} > (10)
+        case !hasLeftSpill && hasRightSpill: {
+          const extraPages = range(endPage + 1, endPage + spillOffset);
+          pages = [...pages, ...extraPages, RIGHT_PAGE];
+          break;
+        }
+
+        // handle: (1) < {4 5} [6] {7 8} > (10)
+        case hasLeftSpill && hasRightSpill:
+        default: {
+          pages = [LEFT_PAGE, ...pages, RIGHT_PAGE];
+          break;
+        }
+      }
+      return [1, ...pages, totalPages];
+    }
+    return range(1, totalPages);
+  };
+
+  render() {
+    if (!this.records || this.totalPages === 1) return null;
+
+    const {currentPage} = this.state;
+    const pages = this.fetchPageNumbers();
+
+    return (
+      <Fragment>
+        <nav aria-label="Countries Pagination">
+          <ul className="pagination">
+            {pages.map((page, index) => {
+              if (page === LEFT_PAGE)
+                return (
+                  <li key={index} className="page-item">
+                    <a
+                      className="page-link"
+                      href="#"
+                      aria-label="Previous"
+                      onClick={this.handleMoveLeft}
+                    >
+                      <span aria-hidden="true">&laquo;</span>
+                      <span className="sr-only">Previous</span>
+                    </a>
+                  </li>
+                );
+
+              if (page === RIGHT_PAGE)
+                return (
+                  <li key={index} className="page-item">
+                    <a
+                      className="page-link"
+                      href="#"
+                      aria-label="Next"
+                      onClick={this.handleMoveRight}
+                    >
+                      <span aria-hidden="true">&raquo;</span>
+                      <span className="sr-only">Next</span>
+                    </a>
+                  </li>
+                );
+
+              return (
+                <li
+                  key={index}
+                  className={`page-item${
+                    currentPage === page ? ' active' : ''
+                  }`}
+                >
+                  <a
+                    className="page-link"
+                    href="#"
+                    onClick={this.handleClick(page)}
+                  >
+                    {page}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </Fragment>
+    );
+  }
+}
+Pagination.propTypes = {
+  records: PropTypes.number.isRequired,
+  limit: PropTypes.number,
+  nextPages: PropTypes.number,
+  onPageChanged: PropTypes.func,
+};
+export default Pagination;
